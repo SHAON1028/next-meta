8c77f5ea9704ddbad3cf74a37f5729f458ed09ca
khairul Islam <khairul.islam@adndiginet.com>
2025-01-09 16:35:50 +0600

    og image local assets

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -3,9 +3,13 @@ import { ImageResponse } from "next/og";
 export const runtime = "edge";
 
 export default async function Image() {
-  const logoSrc = await fetch(new URL("./logo.png", import.meta.url)).then(
-    (res) => res.arrayBuffer()
-  );
+  const logoBuffer = await fetch(
+    new URL("../../public/logo.png", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  // Convert ArrayBuffer to Base64 for embedding
+  const logoBase64 = Buffer.from(logoBuffer).toString("base64");
+  const logoSrc = `data:image/png;base64,${logoBase64}`;
 
   return new ImageResponse(
     (
@@ -14,10 +18,17 @@ export default async function Image() {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#fff",
         }}
       >
-        <img src={logoSrc} height="100" />
+        <img src={logoSrc} style={{ height: 100 }} alt="Logo" />
       </div>
-    )
+    ),
+    {
+      width: 800,
+      height: 600,
+    }
   );
 }
